47457440c4a8113dd076f7efb633ad2ef1300904
Conference-Calls <76670817+Conference-Calls@users.noreply.github.com>
2026-02-08 20:38:33 -0500

    add pistol calibers

--- script.py
+++ script.py
@@ -13,6 +13,18 @@ def try_deepset_attr(obj, key, value):
         for item in obj:
             try_deepset_attr(item, key, value)
 
+def try_deeprm_attr(obj, key):
+    if not isinstance(obj, (dict, list)):
+        return
+    if isinstance(obj, dict):
+        if key in obj.keys():
+            del obj[key]
+        for v in obj.values():
+            try_deeprm_attr(v, key)
+    elif isinstance(obj, list):
+        for item in obj:
+            try_deeprm_attr(item, key)
+
 def main():
     for root, dirs, files in os.walk("."):
         for file in files:
@@ -20,7 +32,8 @@ def main():
                 try:
                     with open(os.path.join(root, file), "r") as f:
                         data = xmltodict.parse(f.read())
-                        try_deepset_attr(data, "ammoClass", "EAC_Bioferrite")
+                        try_deeprm_attr(data, "ThingCategoryDef")
+                        try_deeprm_attr(data, "CombatExtended.AmmoSetDef")
                     with open(os.path.join(root, file), "w") as f:
                         f.write(xmltodict.unparse(data, pretty=True))
                 except Exception as e:
