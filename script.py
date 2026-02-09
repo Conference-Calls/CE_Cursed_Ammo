6fa713966007ead2913384676726b802e51727b1
Conference-Calls <76670817+Conference-Calls@users.noreply.github.com>
2026-02-08 21:05:32 -0500

    remove base bullet

--- script.py
+++ script.py
@@ -30,7 +30,7 @@ def try_deeprm_attr_with_property(obj, property):
         return
     if isinstance(obj, dict):
         if "ThingDef" in obj.keys() and isinstance(obj["ThingDef"], list):
-            obj["ThingDef"] = [thing for thing in obj["ThingDef"] if thing.get(property) != "True"]
+            obj["ThingDef"] = [thing for thing in obj["ThingDef"] if thing.get(property) != "true"]
         for v in obj.values():
             try_deeprm_attr_with_property(v, property)
     elif isinstance(obj, list):
