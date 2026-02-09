9359cf9ef70410a7394e03abd78d0075fe06474b
Conference-Calls <76670817+Conference-Calls@users.noreply.github.com>
2026-02-08 20:32:26 -0500

    Create script.py

---
+++ script.py
@@ -0,0 +1,30 @@
+import xmltodict
+import os
+
+def main():
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file.endswith(".xml"):
+                try:
+                    with open(os.path.join(root, file), "r+") as f:
+                        data = xmltodict.parse(f.read())
+                        try_deepset_attr(data, "ammoClass", "EAC_Bioferrite")
+                        f.write(xmltodict.unparse(data, pretty=True))
+                except Exception as e:
+                    print(f"Failed to process {file}: {e}")
+
+if __name__ == "__main__":
+    main()
+
+def try_deepset_attr(obj, key, value):
+    if not isinstance(obj, (dict, list)):
+        return
+    if isinstance(obj, dict):
+        if key in obj.keys() and isinstance(obj[key], str):
+            obj[key] = value
+        for v in obj.values():
+            try_deepset_attr(v, key, value)
+    elif isinstance(obj, list):
+        for item in obj:
+            try_deepset_attr(item, key, value)
+        
